2421260bcccdbdda1b89d1383ae0e254316f18f6
Ahammod <ahammodullah12@gmail.com>
2024-09-14 17:34:49 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+var myVar=document.querySelector("#pid");
+
+function myMessage1(){
+    myVar.innerHTML="You have clicked on button1";
+    
+}
+
+function myMessage2(){
+     myVar.innerHTML="You have clicked on button2"
+    
+
+}
